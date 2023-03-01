bc5269d40884deb3fcc0089edb0865d578a7773a
jonathan zollinger <jonathan.zollinger@outlook.com>
2023-02-28 20:29:52 -0700

    combine server.go and main.go to one main package

--- main.go
+++ main.go
@@ -4,14 +4,39 @@ import (
 	"os"
 
 	"github.com/Jonathan-Zollinger/Tuirl/internal/cmd"
+
+	"log"
+	"net/http"
+
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/Jonathan-Zollinger/Tuirl/graph"
 )
 
 var version = "dev"
 
+const defaultPort = "7080"
+
 func main() {
+	graphPlayground()
 	cmd.Execute(
 		version,
 		os.Exit,
 		os.Args[1:],
 	)
+}
+
+func graphPlayground() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = defaultPort
+	}
+
+	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{}}))
+
+	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	http.Handle("/query", srv)
+
+	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
